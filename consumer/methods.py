ab6fca009f4ced624c27e5089aa942d277214185
speccy-rom <speccy.rom@gmail.com>
2022-01-04 21:29:51 +0300

    update: methods.py and subscriptions for consumer

--- consumer/methods.py
+++ consumer/methods.py
@@ -1,7 +1,27 @@
+import json
+from producer import methods as producer_methods
+
 async def simple_message(message):
     print("simple_message :: Simple message body is: %r" % message.body)
 
 
 async def simple_message_with_ack(message):
     print("simple_message_with_ack :: Simple message body is: %r" % message.body)
+    await message.channel.basic_ack(message.delivery.delivery_tag)
+
+
+async def chat_message(message):
+
+    incoming_message_dict = json.loads(message.body.decode())
+    incoming_message = incoming_message_dict["message"]
+
+    if len(incoming_message) > 5 and incoming_message[-4:] == "!pow":
+        pass
+    else:
+        outcoming_message = incoming_message[::-1]
+
+    outcoming_message_dict = {}
+    outcoming_message_dict["username"] = "internal_messager"
+    outcoming_message_dict["message"] = outcoming_message
+    await producer_methods.send_message_to_external_main(outcoming_message_dict)
     await message.channel.basic_ack(message.delivery.delivery_tag)
--- consumer/subscriptions.py
+++ consumer/subscriptions.py
@@ -11,17 +11,18 @@ async def consumer_subscriptions():
     channel = await connection.channel()
     cprint(f"AMQP CONSUMER:     ready [yes] PREFIX={UNIQUE_PREFIX}", "green")
 
+
     # cоздадим очередь, в которую будет отправлено сообщение
     # durable значит постоянная очередь (дословный перевод прочная)
-    simple_message_queue__declared = await channel.queue_declare(f"{UNIQUE_PREFIX}:internal__messager:test_message",
-                                                                 durable=False)
+    simple_message_queue__declared = await channel.queue_declare(f"{UNIQUE_PREFIX}:internal__messager:test_message", durable=False)
+    simple_message_with_ack_queue__declared = await channel.queue_declare(f"{UNIQUE_PREFIX}:internal__messager:test_message_with_ack", durable=False)
+    chat_message_queue__declared = await channel.queue_declare(f"{UNIQUE_PREFIX}:internal__messager:chat_message", durable=False)
+
+
     # no_ack=True - сразу ответить брокеру что все ок, сообщение можно удалять из очереди
     await channel.basic_consume(simple_message_queue__declared.queue, methods.simple_message, no_ack=True)
 
-    simple_message_with_ack_queue__declared = await channel.queue_declare(
-        f"{UNIQUE_PREFIX}:internal__messager:test_message_with_ack", durable=False)
-    # no_ack=False - поведение по умолчанию,
-    # отвечаем принудительно в самом обработчике по мере выполенения (предпочитаемый вариант)
-    await channel.basic_consume(simple_message_with_ack_queue__declared.queue, methods.simple_message_with_ack,
-                                no_ack=False)
-    # https://habr.com/ru/post/150134/
+    # no_ack=False - поведение по умолчанию, отвечаем принудительно в самом обработчике по мере выполенения (предпочитаемый вариант)
+    await channel.basic_consume(simple_message_with_ack_queue__declared.queue, methods.simple_message_with_ack, no_ack=False)
+
+    await channel.basic_consume(chat_message_queue__declared.queue, methods.chat_message, no_ack=False)

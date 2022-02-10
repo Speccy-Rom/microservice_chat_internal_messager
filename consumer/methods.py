ff5437911aad40be355241ee80137bbd106d6f46
speccy-rom <speccy.rom@gmail.com>
2022-02-10 23:00:51 +0300

    create: methods send_pow_message for producers consumer

--- consumer/methods.py
+++ consumer/methods.py
@@ -1,6 +1,7 @@
 import json
 from producer import methods as producer_methods
 
+
 async def simple_message(message):
     print("simple_message :: Simple message body is: %r" % message.body)
 
@@ -11,17 +12,22 @@ async def simple_message_with_ack(message):
 
 
 async def chat_message(message):
-
     incoming_message_dict = json.loads(message.body.decode())
     incoming_message = incoming_message_dict["message"]
 
-    if len(incoming_message) > 5 and incoming_message[-4:] == "!pow":
-        pass
+    if "!pow" in incoming_message and incoming_message_dict["source"] == "external__main":
+        outcoming_message_dict = {}
+        outcoming_message_dict["username"] = "internal_messager"
+        outcoming_message_dict["message"] = incoming_message
+        await producer_methods.send_pow_message_to_internal_worker(outcoming_message_dict)
+        await message.channel.basic_ack(message.delivery.delivery_tag)
     else:
-        outcoming_message = incoming_message[::-1]
-
-    outcoming_message_dict = {}
-    outcoming_message_dict["username"] = "internal_messager"
-    outcoming_message_dict["message"] = outcoming_message
-    await producer_methods.send_message_to_external_main(outcoming_message_dict)
-    await message.channel.basic_ack(message.delivery.delivery_tag)
+        if incoming_message_dict["source"] == "external__main":
+            outcoming_message = incoming_message[::-1]
+        elif incoming_message_dict["source"] == "internal__worker":
+            outcoming_message = incoming_message
+        outcoming_message_dict = {}
+        outcoming_message_dict["username"] = "internal_messager"
+        outcoming_message_dict["message"] = outcoming_message
+        await producer_methods.send_message_to_external_main(outcoming_message_dict)
+        await message.channel.basic_ack(message.delivery.delivery_tag)
--- producer/methods.py
+++ producer/methods.py
@@ -6,9 +6,21 @@ from settings import AMQP_URI
 
 
 async def send_message_to_external_main(outcoming_message_dict):
-    cprint(f"AMQP PRODUCER:     send_message_to_external_main", "green")
+    cprint(f"AMQP PRODUCER:     send_message_to_external_main {outcoming_message_dict}", "green")
+    outcoming_message_dict.update({"source": "internal__messager"})
     outcoming_message_bytes = json.dumps(outcoming_message_dict).encode()
     connection = await aiormq.connect(AMQP_URI)
     channel = await connection.channel()
     await channel.basic_publish(outcoming_message_bytes, routing_key=f"{UNIQUE_PREFIX}:external__main:chat_message")
+    await connection.close()
+
+
+async def send_pow_message_to_internal_worker(outcoming_message_dict):
+    cprint(f"AMQP PRODUCER:     send_pow_message_to_internal_worker {outcoming_message_dict}", "green")
+    outcoming_message_dict.update({"source": "internal__messager"})
+    outcoming_message_bytes = json.dumps(outcoming_message_dict).encode()
+    connection = await aiormq.connect(AMQP_URI)
+    channel = await connection.channel()
+    await channel.basic_publish(outcoming_message_bytes,
+                                routing_key=f"{UNIQUE_PREFIX}:internal__worker:pow_chat_message")
     await connection.close()

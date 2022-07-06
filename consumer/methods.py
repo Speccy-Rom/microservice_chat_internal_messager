d20ec539ef919804e4c72e26180d2792355aa88b
Sourcery AI <>
2022-07-06 19:38:09 +0000

    'Refactored by Sourcery'

--- consumer/methods.py
+++ consumer/methods.py
@@ -15,19 +15,16 @@ async def chat_message(message):
     incoming_message_dict = json.loads(message.body.decode())
     incoming_message = incoming_message_dict["message"]
 
+    outcoming_message_dict = {"username": "internal_messager"}
     if "!pow" in incoming_message and incoming_message_dict["source"] == "external__main":
-        outcoming_message_dict = {}
-        outcoming_message_dict["username"] = "internal_messager"
         outcoming_message_dict["message"] = incoming_message
         await producer_methods.send_pow_message_to_internal_worker(outcoming_message_dict)
-        await message.channel.basic_ack(message.delivery.delivery_tag)
     else:
         if incoming_message_dict["source"] == "external__main":
             outcoming_message = incoming_message[::-1]
         elif incoming_message_dict["source"] == "internal__worker":
             outcoming_message = incoming_message
-        outcoming_message_dict = {}
-        outcoming_message_dict["username"] = "internal_messager"
         outcoming_message_dict["message"] = outcoming_message
         await producer_methods.send_message_to_external_main(outcoming_message_dict)
-        await message.channel.basic_ack(message.delivery.delivery_tag)
+
+    await message.channel.basic_ack(message.delivery.delivery_tag)
